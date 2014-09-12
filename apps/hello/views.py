5d229dcdb8b029329ccc81d6ff272eff01a6bb37
krasovskyserge <squidvard@gmail.com>
2014-09-12 18:06:56 +0300

    ticket:1 collect static and add index template

--- apps/hello/views.py
+++ apps/hello/views.py
@@ -1,2 +1,12 @@
+from django.shortcuts import render
+from .models import Person
+from django.template import RequestContext
+
+
 def index(request):
-    pass
+    try:
+        person = Person.objects.get(pk=1)
+    except Person.DoesNotExist:
+        person = Person()
+    request_context = RequestContext(request,  {'person': person})
+    return render(request, 'hello/index.html', request_context)
