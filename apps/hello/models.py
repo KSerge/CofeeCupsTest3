608c7c4054ff8cad4bd0dc5b31dd3a17de3bdd1b
krasovskyserge <squidvard@gmail.com>
2014-09-12 17:17:19 +0300

    ticket:1 add Person model

--- apps/hello/models.py
+++ apps/hello/models.py
@@ -1,1 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Person(models.Model):
+    user = models.OneToOneField(User, null=True)
+    date_of_birth = models.DateField(auto_now=False, null=True, blank=True)
+    bio = models.CharField(max_length=254, null=True, blank=True)
+    jabber = models.EmailField(max_length=100, null=True, blank=True)
+    skype = models.CharField(max_length=50, null=True, blank=True)
+    other_contacts = models.CharField(max_length=254, null=True, blank=True)
