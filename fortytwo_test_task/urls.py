06bdfbdf1d7287a84f00d52fb6d322decdd52111
krasovskyserge <squidvard@gmail.com>
2014-09-12 18:40:48 +0300

    ticket:1 fix mistake in urls after auto remane folder

--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -7,7 +7,7 @@ admin.autodiscover()
 
 urlpatterns = patterns(
     '',
-    url(r'^assets/(.*)$', 'django.views.assets.serve', {'document_root': settings.STATIC_ROOT}),
+    url(r'^static/(.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^hello/', include('apps.hello.urls')),
     url(r'^$', RedirectView.as_view(pattern_name='index'), name='default'),
