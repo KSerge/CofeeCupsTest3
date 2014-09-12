498d6c1843c5d4ba0a322ddde6d0d39883d0e1f1
krasovskyserge <squidvard@gmail.com>
2014-09-12 17:44:33 +0300

    ticket:1 redirect base url to index hello action, add admin to urls

--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 admin.autodiscover()
 
 urlpatterns = patterns(
     '',
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^hello/', include('apps.hello.urls')),
+    url(r'^$', RedirectView.as_view(pattern_name='index'), name='default'),
 )
