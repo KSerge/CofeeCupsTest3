edb3446ec988bfc4929e963fc61aaefccf6ea658
KSerge <squidvard@gmail.com>
2014-09-13 10:51:59 +0300

    ticket:2 add tests for view_requests and middleware

--- apps/hello/tests/test_views.py
+++ apps/hello/tests/test_views.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.core.urlresolvers import reverse
+from ..models import IncomingRequest
 
 
 class HelloAppTestCase(TestCase):
@@ -8,7 +9,10 @@ class HelloAppTestCase(TestCase):
     def test_default_view(self):
         url = reverse('default')
         response = self.client.get(url)
-        self.assertRedirects(response, reverse('index'), status_code=301, target_status_code=200)
+        self.assertRedirects(response,
+                             reverse('index'),
+                             status_code=301,
+                             target_status_code=200)
 
     def test_index_view(self):
         url = reverse('index')
@@ -16,4 +20,15 @@ class HelloAppTestCase(TestCase):
         self.assertTrue('person' in response.context)
         self.assertTrue(response.context['person'].user.first_name == 'Serhij')
         self.assertIn('<h1>42 Coffee Cups Test Assignment</h1>', response.content)
+
+    def test_view_request_view(self):
+        url = reverse('index')
+        for x in range(0, 15):
+            response = self.client.get(url)
+        url = reverse('requests')
+        response = self.client.get(url)
+        self.assertIn('requests', response.context)
+        self.assertTrue(IncomingRequest.objects.all().count() > 10)
+        self.assertTrue(response.context['requests'].count() == 10)
+        self.assertIn('<h4>Requests:</h4>', response.content)
 
