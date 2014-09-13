e1eb6e3d27cc6efb17bb8b711024615f20158f67
KSerge <squidvard@gmail.com>
2014-09-13 18:49:32 +0300

    ticket:8 add template tag to template

--- apps/hello/tests/test_additional_template_view_logic.py
+++ apps/hello/tests/test_additional_template_view_logic.py
@@ -16,6 +16,6 @@ class HelloAppTestCase(TestCase):
     def test_edit_link_template_tag(self):
         person = Person.objects.get(pk=1)
         c = Context({'person': person})
-        test_template = Template("{% load hello_templatestags %} {% edit_link person %}")
+        test_template = Template("{% load hello_templatetags %} {% edit_link person %}")
         rendered = test_template.render(c)
         self.assertIn('<a href="/admin/hello/person/1/">Edit</a>', rendered)
