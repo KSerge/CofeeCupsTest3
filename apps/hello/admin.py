c615e7d5c5addec53c67b34426355ee0a4951753
krasovskyserge <squidvard@gmail.com>
2014-09-18 16:00:31 +0300

    ticket:3 add visiting date to admin list view

--- apps/hello/admin.py
+++ apps/hello/admin.py
@@ -5,6 +5,7 @@ from .models import Person, IncomingRequest, ModelObjectsTracker
 class IncomingRequestAdmin(admin.ModelAdmin):
     fields = ('path', 'priority', 'visiting_date',)
     readonly_fields = ('visiting_date',)
+    list_display = ['path', 'visiting_date', 'priority',]
 
 admin.site.register(Person)
 admin.site.register(IncomingRequest, IncomingRequestAdmin)
