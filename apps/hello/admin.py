348a62e018ecaaa1aeaba33aa5a4a8aaba99862f
krasovskyserge <squidvard@gmail.com>
2014-09-19 17:33:04 +0300

    ticket:10 add all fields for ModelObjectsTrackerAdmin to admin list view

--- apps/hello/admin.py
+++ apps/hello/admin.py
@@ -5,9 +5,15 @@ from .models import Person, IncomingRequest, ModelObjectsTracker
 class IncomingRequestAdmin(admin.ModelAdmin):
     fields = ('path', 'priority', 'visiting_date',)
     readonly_fields = ('visiting_date',)
-    list_display = ['path', 'visiting_date', 'priority',]
+    list_display = ['path', 'visiting_date', 'priority', ]
+
+
+class ModelObjectsTrackerAdmin(admin.ModelAdmin):
+    fields = ('model_name', 'type_of_event', 'created_date',)
+    readonly_fields = ('created_date',)
+    list_display = ['model_name', 'type_of_event', 'created_date', ]
 
 admin.site.register(Person)
 admin.site.register(IncomingRequest, IncomingRequestAdmin)
-admin.site.register(ModelObjectsTracker)
+admin.site.register(ModelObjectsTracker, ModelObjectsTrackerAdmin)
 
